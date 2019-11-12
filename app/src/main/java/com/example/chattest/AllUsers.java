69b353d78de36d30aea67390faf3d38b1eba0f02
Wenzhao <wzhang87@hawk.iit.edu>
2019-11-11 19:23:44 -0600

    Made AllUser.java the start up activity

--- app/src/main/java/com/example/chattest/AllUsers.java
+++ app/src/main/java/com/example/chattest/AllUsers.java
@@ -6,18 +6,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,53 +37,111 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class AllUsers extends AppCompatActivity {
+    private static int SIGN_IN_REQUEST_CODE = 1;
     private DatabaseReference mDatabase;
     private ListView listView;
+    private LinearLayout all_users;
     private ArrayList<UserInformation> arrayList = new ArrayList<>();
     private ArrayList<String> display = new ArrayList<>();
     private ArrayAdapter<UserInformation> adapter;
     private ArrayAdapter<String> displayAdapter;
     private Button search;
+    private Button toChatroom;
     private EditText field;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                display();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.sign_out) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // user is now signed out
+                            startActivity(new Intent(AllUsers.this, AllUsers.class));
+                            finish();
+                        }
+                    });
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        all_users = (LinearLayout) findViewById(R.id.all_users);
         setContentView(R.layout.all_users);
         search = (Button) findViewById(R.id.button4);
+        toChatroom = (Button) findViewById(R.id.button5);
+        toChatroom.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllUsers.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 field = (EditText) findViewById(R.id.editText2);
                 mDatabase.orderByChild("1").equalTo(field.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
-                            Log.d("Testing output2", "PARENT: " + childDataSnapshot.getKey());
-                            Log.d("Testing output2", (String) childDataSnapshot.child("name").getValue());
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                }
+                                                                                                                   @Override
+                                                                                                                   public void onDataChange(DataSnapshot dataSnapshot) {
+                                                                                                                       for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+                                                                                                                           Log.d("Testing output2", "PARENT: " + childDataSnapshot.getKey());
+                                                                                                                           Log.d("Testing output2", (String) childDataSnapshot.child("name").getValue());
+                                                                                                                       }
+                                                                                                                   }
+
+                                                                                                                   @Override
+                                                                                                                   public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                                                                                   }
+                                                                                                               }
                 );
                 mDatabase.orderByChild("2").equalTo(field.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
-                            Log.d("Testing output2", "PARENT: " + childDataSnapshot.getKey());
-                            Log.d("Testing output2", (String) childDataSnapshot.child("name").getValue());
-                            Log.d("Testing output2", (String) childDataSnapshot.child("email").getValue());
+                                                                                                                   @Override
+                                                                                                                   public void onDataChange(DataSnapshot dataSnapshot) {
+                                                                                                                       for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+                                                                                                                           Log.d("Testing output2", "PARENT: " + childDataSnapshot.getKey());
+                                                                                                                           Log.d("Testing output2", (String) childDataSnapshot.child("name").getValue());
+                                                                                                                           Log.d("Testing output2", (String) childDataSnapshot.child("email").getValue());
 
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                }
+                                                                                                                       }
+                                                                                                                   }
+
+                                                                                                                   @Override
+                                                                                                                   public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                                                                                   }
+                                                                                                               }
                 );
             }
         });
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+        } else {
+            display();
+        }
+    }
+    private void display()
+    {
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
         adapter = new ArrayAdapter<UserInformation>(this, android.R.layout.simple_list_item_1, arrayList);
         displayAdapter = new ArrayAdapter<String>(this, layout.simple_list_item_1, display);
@@ -85,7 +151,7 @@ public class AllUsers extends AppCompatActivity {
             //once the button is clicked, it access firebase & retrieve all info about all users
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     UserInformation user = (UserInformation) dataSnapshot.getValue(UserInformation.class);
                     String getEmail = ds.child("email").getValue(String.class);
                     user.setEmail(getEmail);
@@ -102,11 +168,13 @@ public class AllUsers extends AppCompatActivity {
                     adapter.notifyDataSetChanged();
                 }
             }
+
             @Override
-            public void onCancelled(DatabaseError databaseError) {}
+            public void onCancelled(DatabaseError databaseError) {
+            }
         };
         mDatabase.addListenerForSingleValueEvent(eventListener);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 UserInformation usertest = adapter.getItem(position);
@@ -124,6 +192,5 @@ public class AllUsers extends AppCompatActivity {
 
             }
         });
-
     }
 }
